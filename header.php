2da37d5e79e4f24c778b42b1a8edc3bec0d24f8b
Henning Huth <henzgarthebarbar@gmail.com>
2020-04-18 16:04:40 +0200

    changed the nav-color and added the container-fluid class

--- header.php
+++ header.php
@@ -25,49 +25,51 @@
 <div id="page" class="site">
 	<a class="sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'marup' ); ?></a>
 
-	<header id="masthead" class="site-header">		
-	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-        <ul class="navbar-nav mr-auto">
-		<?php
-                wp_nav_menu( array(
-                    'theme_location'    => 'menu-2',
-                    'menu_id'           => 'primary-menu',
-                    'depth'             => 2,
-                    'container'         => 'div',
-                    'container_class'   => 'collapse navbar-collapse',
-                    'container_id'      => 'bs-example-navbar-collapse-1',
-                    'menu_class'        => 'nav navbar-nav',
-                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                    'walker'            => new WP_Bootstrap_Navwalker(),
-                ) );
-            ?>
-        </ul>
-    </div>
-    <div class="mx-auto order-0">
-        <a class="navbar-brand mx-auto" href="#">Navbar 2</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    </div>
-    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-        <ul class="navbar-nav ml-auto">
-		<?php
-                wp_nav_menu( array(
-                    'theme_location'    => 'menu-1',
-                    'menu_id'           => 'primary-menu',
-                    'depth'             => 2,
-                    'container'         => 'div',
-                    'container_class'   => 'collapse navbar-collapse',
-                    'container_id'      => 'bs-example-navbar-collapse-1',
-                    'menu_class'        => 'nav navbar-nav',
-                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                    'walker'            => new WP_Bootstrap_Navwalker(),
-                ) );
-            ?>
-        </ul>
-    </div>
-</nav>
+	<header id="masthead" class="site-header">
+		<div class="container-fluid">		
+			<nav class="navbar navbar-expand-md navbar-light ">
+				<div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+					<ul class="navbar-nav mr-auto">
+					<?php
+							wp_nav_menu( array(
+								'theme_location'    => 'menu-2',
+								'menu_id'           => 'primary-menu',
+								'depth'             => 2,
+								'container'         => 'div',
+								'container_class'   => 'collapse navbar-collapse',
+								'container_id'      => 'bs-example-navbar-collapse-1',
+								'menu_class'        => 'nav navbar-nav',
+								'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+								'walker'            => new WP_Bootstrap_Navwalker(),
+							) );
+						?>
+					</ul>
+				</div>
+				<div class="mx-auto order-0">
+					<a class="navbar-brand mx-auto" href="#">Navbar 2</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+				</div>
+				<div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+					<ul class="navbar-nav ml-auto">
+					<?php
+							wp_nav_menu( array(
+								'theme_location'    => 'menu-1',
+								'menu_id'           => 'primary-menu',
+								'depth'             => 2,
+								'container'         => 'div',
+								'container_class'   => 'collapse navbar-collapse',
+								'container_id'      => 'bs-example-navbar-collapse-1',
+								'menu_class'        => 'nav navbar-nav',
+								'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+								'walker'            => new WP_Bootstrap_Navwalker(),
+							) );
+						?>
+					</ul>
+				</div>
+			</nav>
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
